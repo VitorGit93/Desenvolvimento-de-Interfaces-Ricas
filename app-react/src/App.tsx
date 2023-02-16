33753c4bd4de6067a92c7b47a1d43c2998bfe923
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-02-16 19:52:55 -0400

    ⬆️ Upload de novos arquivos atualizados

--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -1,25 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-
   return (
-    <div className="app">
-
-      <header>
-        <h1>Bem Vindo</h1>
-      </header>
-
-      <main>
-
-        <div className="action-buttons">
-          <a className="btn" href="/login.html">Login</a>
-          <a className="btn" href="/register.html">Criar Conta</a>
-        </div>
-        
-      </main>
-
+    <div id="App">
+       <header>
+            <h1>Bem Vindo</h1>
+        </header>
+        <main>
+            <div className="action-buttons">
+                <a className="btn btn-primary" href="/login.html">Login</a>
+                <a className="btn" href="/register.html">Criar Conta</a>  
+            </div>
+        </main>
     </div>
   )
 }
