518d7b170dcb5f4fbb983cf7187364fcfe395344
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-02-16 20:23:18 -0400

    Criando um Component Button

--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -1,17 +1,19 @@
 import './App.css'
+import { Button } from './components/Button'
 
 function App() {
   return (
     <div id="App">
-       <header>
-            <h1>Bem Vindo</h1>
-        </header>
-        <main>
-            <div className="action-buttons">
-                <a className="btn btn-primary" href="/login.html">Login</a>
-                <a className="btn" href="/register.html">Criar Conta</a>  
-            </div>
-        </main>
+      <header>
+        <h1>Bem Vindo</h1>
+      </header>
+      <main>
+        <div className="action-buttons">
+          <Button text="Login" />
+          <Button text="Cadastro" />
+          <Button text="" />
+        </div>
+      </main>
     </div>
   )
 }
