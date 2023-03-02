9e9083cb91bb7e2b27fcc0d8235dd35f251b5697
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-03-01 21:32:47 -0400

    Footer navbar-menu atualizado

--- app-react/src/components/Footer/index.tsx
+++ app-react/src/components/Footer/index.tsx
@@ -1,14 +1,13 @@
-import { FiSearch, FiHome, FiMenu  } from 'react-icons/all';
+import { FiSearch, FiHome, FiMenu, FiBookmark  } from 'react-icons/all';
 
 
 export function Footer() {
     return (
         <footer className="navbar-menu">
-
-            <FiHome size={24} color='blue'/>
-            <FiSearch size={24} color='blue' />
-            <FiMenu size={24} color='blue' />
-
+            <FiHome size={20} color='blue'/>
+            <FiSearch size={20} color='blue' />
+            <FiBookmark size={20} color='blue' />
+            <FiMenu size={20} color='blue' />
         </footer>
     )
 }
