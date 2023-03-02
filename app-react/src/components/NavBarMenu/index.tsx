64294e3ebca09d7603c8e58b2532d6e25c83fa0f
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-03-01 22:01:01 -0400

    Adicionando mais ícones ao navbar-menu

--- app-react/src/components/NavBarMenu/index.tsx
+++ app-react/src/components/NavBarMenu/index.tsx
@@ -1,4 +1,4 @@
-import { House, User, Gear as Settings} from 'phosphor-react'
+import { House, User, Gear as Settings, List, Share} from 'phosphor-react'
 import './style.css'
 
 
@@ -6,18 +6,27 @@ export function NavBarMenu() {
     return(
         <>
             <nav className="navbar-menu">
+                <a title="Perfil" href="#">
+                    <User size={24} />
+                </a>
+
                 <a title="Início" href="#">
                     <House size={24} />
                 </a>
 
-                <a title="Perfil" href="#">
-                    <User size={24} />
-                </a>
 
                 <a title="Configurações" href="#">
                     <Settings size={24} />
                 </a>
 
+                <a title="Compartilhar" href="#">
+                    <Share size={24} />
+                </a>
+
+                <a title="Menu" href="#">
+                    <List size={24} />
+                </a>
+                
             </nav>
         
         </>
