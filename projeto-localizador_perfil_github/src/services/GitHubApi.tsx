4cd13c14866bc532fa40a8475a5e6bce25ce08b9
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-04-06 21:12:51 -0400

    Mudando o nome da função da API

--- projeto-localizador_perfil_github/src/services/GitHubApi.tsx
+++ projeto-localizador_perfil_github/src/services/GitHubApi.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
-export const getUserProfile = async (username: any) => {
+export const getPerfil = async (username: any) => {
     const response = await axios.get(`${BASE_URL}/users/${username}`);
     return response.data;
 };
