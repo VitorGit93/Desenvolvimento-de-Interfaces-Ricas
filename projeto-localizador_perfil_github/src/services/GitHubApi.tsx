4000bba740fef829e69c0588f28c27278ba55dd8
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-04-12 22:51:14 -0400

    Usando o AppBar Component

--- projeto-localizador_perfil_github/src/services/GitHubApi.tsx
+++ projeto-localizador_perfil_github/src/services/GitHubApi.tsx
@@ -1,3 +1,5 @@
+// Aqui está a chamada da API do Github, quen retorna os dados do usuário
+
 import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
@@ -6,3 +8,5 @@ export const getPerfil = async (username: any) => {
     const response = await axios.get(`${BASE_URL}/users/${username}`);
     return response.data;
 };
+
+// token Github API: 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
