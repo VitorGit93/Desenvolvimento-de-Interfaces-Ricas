31d0574ae0e18ba828dac14ab6af155f13d5d987
VitorGit93 <vitor.santos9@estudante.ifms.edu.br>
2023-04-20 01:55:52 -0400

    Implementando um tratamento de erro na API

--- projeto-localizador_perfil_github/src/services/GitHubApi.tsx
+++ projeto-localizador_perfil_github/src/services/GitHubApi.tsx
@@ -4,9 +4,14 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
-export const getPerfil = async (username: any) => {
-    const response = await axios.get(`${BASE_URL}/users/${username}`);
-    return response.data;
+export const getPerfil = async (username: string) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/users/${username}`);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
 };
+
 
 // token Github API: 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
