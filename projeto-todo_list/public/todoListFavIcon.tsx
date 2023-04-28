12f4db3a075c422ba0e5c10d975ae34e748023a3
VitorGit93 <vitor.santos9@estudante.ifms.edu.br>
2023-04-28 00:55:05 -0400

    🚧 Aterando o favicon da guia

---
+++ projeto-todo_list/public/todoListFavIcon.tsx
@@ -0,0 +1,11 @@
+import SvgIcon from '@mui/material/SvgIcon';
+import React from 'react';
+
+
+function MeuIcone(props) {
+    return (
+        <SvgIcon {...props}>
+            <path d="M10 10h10v10H10z" />
+        </SvgIcon>
+    );
+}
