2cbbd6aa7117576a745fb5dabbc4d208cf7c6bde
VitorGit93 <vitor.santos9@estudante.ifms.edu.br>
2023-05-03 22:46:48 -0400

    Implementando o TextField e o Button no Grid

--- projeto-todo_list/src/App.tsx
+++ projeto-todo_list/src/App.tsx
@@ -1,15 +1,15 @@
-import { AppBar, CssBaseline, ThemeProvider, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, Container, CssBaseline, Grid, TextField, ThemeProvider, Toolbar, Typography } from '@mui/material'
 import { darkTheme } from './Themes'
 import { useTheme } from '@mui/material/styles'
 
 
 function App() {
-const theme = useTheme()
+  const theme = useTheme()
   return (
     <>
       <ThemeProvider theme={darkTheme}>
 
-        <CssBaseline/>
+        <CssBaseline />
         <AppBar position="static">
           <Toolbar
             sx={{
@@ -26,6 +26,23 @@ const theme = useTheme()
             <Typography variant="h5" component="h1">todo</Typography>
           </Toolbar>
         </AppBar>
+
+        <main>
+          <Container>
+            <Grid container spacing={0.5}>
+
+              <Grid item xs={10} sm={12}>
+                <TextField name="task" fullWidth />
+
+              </Grid>
+              <Grid item xl={2} sm={12}>
+                <Button variant="contained" fullWidth>Criar</Button>
+
+              </Grid>
+
+            </Grid>
+          </Container>
+        </main>
 
       </ThemeProvider>
     </>
