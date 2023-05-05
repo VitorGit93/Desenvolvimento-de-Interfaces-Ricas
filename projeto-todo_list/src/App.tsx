6ab6e7fd572fe2c2d6d25031db97fc9b575c4347
VitorGit93 <vitor.santos9@estudante.ifms.edu.br>
2023-05-04 21:04:38 -0400

    Implemantando o grid das tarefas criadas

--- projeto-todo_list/src/App.tsx
+++ projeto-todo_list/src/App.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Button, Container, CssBaseline, Grid, TextField, ThemeProvider, Toolbar, Typography, colors } from '@mui/material'
+import { AppBar, Badge, Button, Container, CssBaseline, Grid, TextField, ThemeProvider, Toolbar, Typography, colors } from '@mui/material'
 import { darkTheme } from './Themes'
 import { useTheme } from '@mui/material/styles'
 import { CheckFat, PlusCircle } from '@phosphor-icons/react'
@@ -32,7 +32,7 @@ function App() {
                 color: theme.palette.primary.light,
               }}
             >
-              <CheckFat size = {32} weight = 'fill'/>Lista de Tarefas
+              <CheckFat size={32} weight='fill' />Lista de Tarefas
             </Typography>
           </Toolbar>
         </AppBar>
@@ -51,7 +51,7 @@ function App() {
             >
 
               <Grid item xl={10} sm={12}>
-                <TextField variant='outlined' name="task" fullWidth 
+                <TextField variant='outlined' name="task" fullWidth
                   sx={{
                     backgroundColor: colors.grey[800],
                   }}
@@ -59,18 +59,36 @@ function App() {
               </Grid>
 
               <Grid item xl={2} sm={12}>
-
                 <Button variant="contained" fullWidth
                   sx={{
                     height: '100%',
                   }}
                 >
-                  <span> Criar </span> <PlusCircle size={32}/>
+                  <span> Criar </span> <PlusCircle size={32} />
                 </Button>
-
               </Grid>
 
             </Grid>
+
+            <Grid container spacing={theme.spacing(1)}>
+
+              <Grid item>
+                <Typography variant='caption'
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: theme.spacing(1),
+                  }}
+                >
+                  <span>Tarefas Criadas</span>
+                  <Badge color="primary" badgeContent={0} showZero></Badge>
+                </Typography>
+              </Grid>
+
+            </Grid>
+
+
+
           </Container>
         </main>
 
