eb61fe9a48106f6ff63976e3c2ac46ee966f9817
VitorGit93 <vitor.santos9@estudante.ifms.edu.br>
2023-05-11 20:46:30 -0400

    Atualizando dependências e fazendo correções

--- projeto-todo_list/src/App.tsx
+++ projeto-todo_list/src/App.tsx
@@ -1,100 +1,37 @@
-import { AppBar, Badge, Button, Container, CssBaseline, Grid, TextField, ThemeProvider, Toolbar, Typography, colors } from '@mui/material'
-import { darkTheme } from './Themes'
-import { useTheme } from '@mui/material/styles'
-import { CheckFat, PlusCircle } from '@phosphor-icons/react'
-
+import { Container, CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme } from './Themes';
+import { HeaderCabecalho } from './components/HeaderCabecalho';
+import { TaskInput } from './components/TaskInput';
+import TaskList from './components/TaskList';
 
 function App() {
-  const theme = useTheme()
-  return (
-    <>
-      <ThemeProvider theme={darkTheme}>
+    return (
+        <ThemeProvider theme={darkTheme}>
 
-        <CssBaseline />
-        <AppBar position="static">
-          <Toolbar
-            sx={{
-              paddingTop: theme.spacing(2),
-              paddingBottom: theme.spacing(2),
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              '@media all': {
-                minHeight: 200,
-              }
-            }}
-          >
-            <Typography variant="h5" component="h1"
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: theme.spacing(2),
-                color: theme.palette.primary.light,
-              }}
-            >
-              <CheckFat size={32} weight='fill' />Lista de Tarefas
-            </Typography>
-          </Toolbar>
-        </AppBar>
+            <CssBaseline />
 
-        <main>
-          <Container
-            sx={{
-              position: 'relative',
-            }}
-          >
-            <Grid container spacing={0.5}
-              sx={{
-                position: 'absolute',
-                top: '-29px',
-              }}
-            >
+            <HeaderCabecalho />
 
-              <Grid item xl={10} sm={12}>
-                <TextField variant='outlined' name="task" fullWidth
-                  sx={{
-                    backgroundColor: colors.grey[800],
-                  }}
-                />
-              </Grid>
+            <main>
 
-              <Grid item xl={2} sm={12}>
-                <Button variant="contained" fullWidth
-                  sx={{
-                    height: '100%',
-                  }}
-                >
-                  <span> Criar </span> <PlusCircle size={32} />
-                </Button>
-              </Grid>
+                <Container
+                 sx={{
+                    position: 'relative',
+                    top: '-27px',
+                    //background: 'linear-gradient(to right, #005c97, #363795)'
+                    
+                }}>
 
-            </Grid>
+                    <TaskInput />
 
-            <Grid container spacing={theme.spacing(1)}>
+                    <TaskList />
 
-              <Grid item>
-                <Typography variant='caption'
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: theme.spacing(1),
-                  }}
-                >
-                  <span>Tarefas Criadas</span>
-                  <Badge color="primary" badgeContent={0} showZero></Badge>
-                </Typography>
-              </Grid>
+                </Container>
 
-            </Grid>
+            </main>
 
-
-
-          </Container>
-        </main>
-
-      </ThemeProvider>
-    </>
-  )
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
